d26b680cfa4cf213b9036975a36028d4c90ec427
warrend <warrend555@gmail.com>
2017-08-20 15:36:38 -0400

    add router to app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as actions from '../actions/bookActions';
 import Books from '../components/Books';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   componentDidMount() {
@@ -16,13 +21,37 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to YouShouldRead</h2>
         </div>
-        <p className="App-intro">
-        </p>
-        <Books books={this.props.books} />
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/new">Add a Book</Link></li>
+              <li><Link to="/books">All Books</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/new" component={About}/>
+            <Route path="/books" render={props => <Books books={this.props.books} />}/>
+          </div>
+        </Router>
       </div>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
 
 const mapStateToProps = (state) => {
   return {
