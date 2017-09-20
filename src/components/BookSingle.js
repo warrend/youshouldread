b01a36acc94ec5dd1aa0a2b1c6429f864d90441b
warrend <warrend555@gmail.com>
2017-09-20 15:07:28 -0400

    clean up and add heart icon

--- src/components/BookSingle.js
+++ src/components/BookSingle.js
@@ -5,21 +5,15 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
 class BookSingle extends Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	// this.state = {likes: 0}
-	// }
-
-	// addLike = () => {
-	// 	this.setState({likes: this.state.likes + 1})
-	// }
 
 	render() {
 		const likesCount = this.props.book.likes + 1
 		return (
 			<div>
 				<p onClick={() => this.props.selectBook(this.props.book)}>{this.props.book.title}</p>
-				<RaisedButton className="btn" onClick={() => this.props.addLike(this.props.book.id, likesCount)}>Like</RaisedButton><span> {this.props.book.likes}</span>
+				<RaisedButton className="btn" onClick={() => this.props.addLike(this.props.book.id, likesCount)}>Like</RaisedButton><span> &#9829; {this.props.book.likes}</span>
+				<br />
+				<br />
 			</div>
 		)
 	}
