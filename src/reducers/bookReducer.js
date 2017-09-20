f24d5c842164aab27f6654953805ca353ec29002
warrend <warrend555@gmail.com>
2017-09-20 15:02:01 -0400

    attempted to add spread op to reducer

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -2,7 +2,9 @@ export default function booksReducer(state = [], action) {
   switch ( action.type ) {
     case 'FETCH_BOOKS':
       return action.payload
-    case 'ADD_LIKE': {
+    case 'ADD_LIKE': 
+      // return [...state.slice(0, action.bookId), {...state[action.bookId], likes: action.likes}, ...state.slice(action.bookId + 1)]
+      {
       return state.map(book => {
         if (book.id !== action.bookId) {
           return book
